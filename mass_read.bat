750c08305457d49cde02306a31b3561868c0f975
Sean Byler <seanbyler12@gmail.com>
2023-02-26 13:57:16 -0600

    Displays graph after measurement

--- mass_read.bat
+++ mass_read.bat
@@ -3,10 +3,16 @@
 set loop=0
 :loop
 set /a loop=%loop%+1
-"Spectrometer Reader.exe" -subject subject -subtype young -wavelength 400 -healthy yes -read -iterations 20 -nobinary
-if "%loop%"=="10" goto end
+"Spectrometer Reader.exe" -subject soybean_leaf -subtype young -wavelength 0 -healthy yes -read -iterations 25 -nobinary
+if "%loop%"=="1" goto end
 goto loop
 
 :end
 echo Finished.
-pause
+
+FOR /F "eol=| delims=" %%I IN ('DIR "Data\*.csv" /A-D /B /O-D /TW 2^>nul') DO SET "NewestFile=%%I" & GOTO FoundFile
+ECHO No *.jpg file found!
+GOTO :EOF
+
+:FoundFile
+python Data\spectrograph_viewer.py Data\%NewestFile%
